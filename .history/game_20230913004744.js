e78c616eda0bcba47e0fcbcce2f1f222c90a79b9
Defne Turğut <defneew@outlook.com>
2023-09-13 02:13:18 +0300

    simon game
    web based simon game with javascript

---
+++ .history/game_20230913004744.js
@@ -0,0 +1,4 @@
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random * 3);
+    console.log(randomNumber);
+}
---
+++ .history/game_20230913013016.js
@@ -0,0 +1,54 @@
+var buttoncolours = ["red","blue","green","yellow"];
+var gamePattern = [];
+
+
+var userClickedPattern = [];
+var started = false;
+var level = 0;
+
+$(document).keypress(function(){
+    if(!started){
+        $(#level-title).text("Level" + level);
+        nextSequence();
+        started = true;
+
+    }
+});
+
+$(".btn").click(function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length-1);
+});
+function checkAnswer(currentLevel){
+    
+}
+function nextSequence(){
+    level++;
+    $("#level-title").text("Level " + level);
+    var randomNumber = Math.floor(Math.random * 4);
+    var randomChosenColour = buttoncolours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour);
+}
+
+function playSound(name){
+    
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("presseed");
+
+    setTimeout(function(){
+        $("#" + currentColour).removeClass("presseed");
+
+    },100);
+}
